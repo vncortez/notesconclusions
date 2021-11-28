49fec102bbd4bc0a30af0d93bde7a0cebdafcc6b
Vinicius Cortez <viniciusecortez@gmail.com>
2021-11-28 05:19:21 -0300

    Adiciona secret_key como variável de ambiente

--- NotesConclusions/settings.py
+++ NotesConclusions/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 DEBUG = eval(os.getenv('DEBUG', 'False'))
