e30bb990029fd6c0294dd7a8583e0d66d690a26a
Vinicius Cortez <viniciusecortez@gmail.com>
2021-11-28 04:49:44 -0300

    Criadas models e migrations

--- Text/models.py
+++ Text/models.py
@@ -1,23 +1,34 @@
+import re
 from django.db import models
-from django.db.models.fields import TextField
-
+from django.contrib.auth import get_user_model
 # Create your models here.
 
 
 
-class Title (models.Model):
-    class TypeOf(models.Choices):
+class Title(models.Model):
+    class TypeOf(models.TextChoices):
         a = '#'
         b = '##'
         c = '###'
     text = models.CharField(max_length=30)
     order = models.IntegerField()
-    type_of = models.CharField(choices=TypeOf.choices())
+    type_of = models.CharField(max_length=3, choices=TypeOf.choices)
 
 class TypeOfText(models.Model):
     text = models.CharField(max_length=30)
 
-
 class NodeText(models.Model):
-    text = models.TextField(max_length=500)
-    models.ForeignKey("app.Model", verbose_name=_(""), on_delete=models.CASCADE)
+    text = models.TextField(max_length=1500)
+    titulos = models.ManyToManyField(to=Title)
+    tipo_texto = models.ForeignKey(to=TypeOfText, on_delete=models.CASCADE, related_name='textos')
+    usuario = models.ForeignKey(to=get_user_model(), on_delete=models.CASCADE)
+    
+    
+    @property
+    def author(self):
+        return self.usuario.username
+    
+    @property
+    def type_of_text(self):
+        return self.tipo_texto.text
+    
